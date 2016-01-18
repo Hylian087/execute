1addd331a76b70e05536c85eddf7906b9530dedc
Phuc-Dang <atlaswasright087@gmail.com>
2016-01-18 15:03:06 +0200

    Mise à jour des Inputs du jeu

--- Execute/Assets/Scripts/PlayerInputs.cs
+++ Execute/Assets/Scripts/PlayerInputs.cs
@@ -6,14 +6,30 @@ public class PlayerInputs : MonoBehaviour {
 
 	int personalScore;
 	Time rythm;
-	//List<T> inputsTable; 
+	string[] p1Inputs;
+	int randomNumber;
+	string randomSelectedInput;
+	string[] p1Selected;
+	int maxInputs = 3;
 
 	// Use this for initialization
 	void Start () {
+		p1Inputs = new string[]{"Joy1A", "Joy1B", "Joy1X", "Joy1Y"};
+		for(int i = 0; i <= maxInputs; i++){
+			randomNumber = Random.Range (0,4);
+			Debug.Log(p1Selected[randomNumber]);
+			p1Selected = new string[]{p1Inputs[randomNumber]};
+
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (Input.GetAxisRaw("Up"));
+
+
+		/*if(Input.GetButtonDown("Joy1A")){
+			Debug.Log ("Joy1A");
+		}*/
+
 	}
 }
