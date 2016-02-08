835538c5971db8c37801c0c983d3031f4f45ad24
Etienne Gauvin <etienne@gauvin.info>
2016-02-08 23:12:03 +0100

    Améliorations de la classe Joypad
    Gestion du D-Pad comme 4 boutons et non deux axes.

--- Execute/Assets/Scripts/JoyTest.cs
+++ Execute/Assets/Scripts/JoyTest.cs
@@ -7,21 +7,40 @@ public class JoyTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
+		// Création de 4 joypads
 		for (int j = 0; j < 4; j++) {
-			Debug.Log (j);
 			joypads[j] = new Joypad();
+		}
+		
+		
+		string[] inputNames = Input.GetJoystickNames();
+		
+		foreach (string name in inputNames) {
+			Debug.Log(name);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
+		// Mise à jour nécessaire pour le D-Pad
+		Joypad.UpdateAll();
+		
+		// Boucle sur les manettes
 		foreach (Joypad joypad in joypads) {
-			if (joypad.isDown(Joypad.A)) {
-				Debug.Log("Joueur " + joypad.getID() + " appuie sur A");
-			}
-			else if (joypad.isInverseDown(Joypad.A)) {
-				Debug.Log("Joueur " + joypad.getID() + " appuie sur l'inverse de A (Y)");
+			//joypad.Update();
+			
+			// Boucle sur les boutons qui nous intéressent (sur les axes)
+			foreach (string button in Joypad.AXIS_BUTTONS) {
+				
+				if (joypad.IsDown(button)) {
+					Debug.Log("Joueur " + joypad.getID() + " appuie sur " + button);
+				}
+				// else if (joypad.IsInverseDown(button)) {
+				// 	Debug.Log("Joueur " + joypad.getID() + " appuie sur l'inverse de " + button);
+				// }
+				
 			}
 		}
 
--- Execute/Assets/Scripts/Joypad.cs
+++ Execute/Assets/Scripts/Joypad.cs
@@ -1,81 +1,165 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+/**
+ * Classe représentant une manette.
+ */
 public class Joypad {
-
-	// Constantes des boutons
-	public const int UP = 0;
-	public const int DOWN = 1;
-	public const int LEFT = 2;
-	public const int RIGHT = 3;
-
-	public const int A = 4;
-	public const int B = 5;
-	public const int X = 6;
-	public const int Y = 7;
-
-	public const int START = 8;
-
-	private string[] buttonsNames = new string[] { "Up", "Down", "Left", "Right", "A", "B", "X", "Y", "Start" };
-
-	private int[] buttonsInverses = new int[] { UP, DOWN, RIGHT, LEFT, B, A, Y, X };
-
-
+	
+	// Liste des joypads instanciés
+	private static List<Joypad> joypads = new List<Joypad>();
+	
+	// Liste des boutons des axes (const)
+	public static string[] AXIS_BUTTONS = new string[] {"Up", "Down", "Left", "Right", "A", "B", "X", "Y"};
+	
+	// Tableau des touches inversées
+	private Dictionary<string, string> inverses;
+    
 	// Identifiant de la dernière instance
-	private static int _lastId = 0;
-
+	private static int lastId = 0;
+	
 	// Identifiant de l'instance actuelle
-	private int _id;
-
-	// 
-	private bool _isPluggedIn = false;
-
-	// Si le 
+	private int id;
+	
+	// Axes à traiter comme des boutons (D-Pad)
+	private Dictionary<string, int> axesButtons;
 
 	/**
 	 * Initialisation de l'objet
 	 */
 	public Joypad() {
-
+		
+		// Tableau associatif des touches inverses
+		inverses = new Dictionary<string, string>();
+		
+		inverses.Add("Up", 		"Down");
+		inverses.Add("Right", 	"Left");
+		inverses.Add("Left", 	"Right");
+		inverses.Add("Down", 	"Up");
+		
+		inverses.Add("A", 		"Y");
+		inverses.Add("B", 		"X");
+		inverses.Add("X", 		"B");
+		inverses.Add("Y", 		"A");
+		
+		// Initialisation des axes à traiter comme des boutons (D-Pad)
+		axesButtons = new Dictionary<string, int>();
+		
+		axesButtons.Add("Up", 		0);
+		axesButtons.Add("Down", 	0);
+		axesButtons.Add("Left", 	0);
+		axesButtons.Add("Right", 	0);
+		
 		// Changement de Joy pour la prochaine instance
-		_lastId++;
+		lastId++;
 
 		// Initialisation de l'identifiant
-		_id = _lastId;
+		id = lastId;
+		
+		// Ajout de ce joypad à la liste des joypads
+		joypads.Add(this);
 	}
-	
+
 	/**
 	 * Tester si un bouton est appuyé
 	 * 
+	 * @param <string> le nom de la touche ("A", "B", "Up", ...)
+	 * @return <bool>
 	 */
-	public bool isDown(int button) {
-		return Input.GetButtonDown ("Joy" + _id + buttonsNames[button]);
+	public bool IsDown(string button) {
+		float axis;
+		
+		
+		switch (button) {
+			
+			case "Up":
+			case "Down":
+			case "Left":
+			case "Right":
+				// Pour détecter juste une pression
+				return axesButtons[button] == 1;
+			break;
+			
+			default:
+				return Input.GetButtonDown("Joy" + id + button);
+			break;
+		}
 	}
-	
-	
+
+
 	/**
-	 * Tester si un bouton est appuyé
+	 * Tester si l'inverse d'un bouton est appuyé
+	 * (ne fonctionne que pour les touches directionnelles et ABXY)
 	 * 
+	 * @param <string> le nom de la touche ("A", "B", "Up", ...)
+	 * @return <bool>
 	 */
-	public bool isInverseDown(int button) {
-		return Input.GetButtonDown ("Joy" + _id + buttonsNames[buttonsInverses[button]]);
+	public bool IsInverseDown(string button) {
+		return IsDown(inverses[button]);
 	}
 
-
-	
 	/**
-	 * Tester si la manette est branchée
-	 * (si un bouton a été pressé au moins une fois)
-	 */
-	public bool isPluggedIn() {
-		return _isPluggedIn;
-	}
-
-
-	/**
-	 * Retourne l'ID
+	 * Retourne l'ID du joypad
+	 * 
+	 * @return <int>
 	 */
 	public int getID() {
-		return this._id;
+		return id;
+	}
+	
+	
+	/**
+	 * Mise à jour
+	 */
+	public void Update() {
+		// Met à jour le tableau des axes utilisées
+		List<string> buttons = new List<string> (axesButtons.Keys);
+		
+		foreach (string button in buttons) {
+			float axis;
+			
+			switch (button) {
+				case "Up":
+				case "Down":
+					axis = Input.GetAxis("Joy" + id + "UpDown");
+					
+					// Le bouton Haut ou Bas est pressé
+					if ((button == "Up" 	&& axis == 1) ^
+						(button == "Down" 	&& axis == -1)) {
+						axesButtons[button]++;
+					}
+					else {
+						axesButtons[button] = 0;
+					}
+					
+				break;
+					
+				case "Left":
+				case "Right":
+					axis = Input.GetAxis("Joy" + id + "RightLeft");
+					
+					// Le bouton Droite ou Gauche est pressé
+					if ((button == "Left" 	&& axis == 1) ^
+						(button == "Right" 	&& axis == -1)) {
+						axesButtons[button]++;
+					}
+					else {
+						axesButtons[button] = 0;
+					}
+				break;
+			}
+		}
+	}
+	
+	
+	/**
+	 * Mise à jour des toutes les instances de Joypad
+	 */
+	public static void UpdateAll() {
+		// Met à jour le tableau des axes utilisées
+		foreach (Joypad joypad in joypads) {
+			joypad.Update();
+		}
 	}
 }
