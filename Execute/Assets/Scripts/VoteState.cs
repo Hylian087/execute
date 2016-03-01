24a29e0f749912e765bfa4a46a1299e57e0a473a
Hylian087 <atlaswasright087@gmail.com>
2016-03-01 19:46:36 +0100

    Refactorisation et commentaires du code

--- Execute/Assets/Scripts/VoteState.cs
+++ Execute/Assets/Scripts/VoteState.cs
@@ -10,21 +10,31 @@ public class VoteState : MonoBehaviour {
 
 	// Timer du vote
 	public float voteDuration = 5.0f;
+
 	// Temps passé sur le vote
 	public float currentTime;
+
 	// Le vote a-t-il commencé?
 	public static bool voteStarted = false;
-	// Les votes sont-ils terminés?
+	// Les votes ont-ils été comptés? (Vote terminé?)
 	public bool votesCounted;
-	public Dictionary<string,GameObject> voteCounter;
-	Dictionary<int, GameObject> voteObject = new Dictionary<int, GameObject>();
 
-
+	// Personnes qui ont voté
 	public int hasVoted;
 
+	// Compteurs de votes (un pour chaque joueur), string = nom du joueur, GameObject = objet créé en jeu pour afficher le compteur
+	public Dictionary<string,GameObject> voteCounter = new Dictionary<string,GameObject>();
+
+	// Dictionnaire contenant les feedback visuel des votes de chaque joueur (int = ID du joueur, GameObject = crane)
+	Dictionary<int, GameObject> voteSkull = new Dictionary<int, GameObject>();
+
+	// Tableau décomptant les votes : string = nom du joueur, int = nombre de votes
+	public Dictionary<string, int> votes;
+
+	// Instance du script
 	static VoteState vs;
 
-	public Dictionary<string, int> votes;
+
 
 	/**
 	 * Créer une voteState
@@ -46,14 +56,16 @@ public class VoteState : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Création du tableau comptant les votes
 		votes = new Dictionary<string, int> (4);
-		voteCounter = new Dictionary<string,GameObject>();
 
+		// Création des compteurs de votes
 		foreach(Player player in game.players){
 			votes.Add (player.voteID,0);
 			voteCounter.Add(player.voteID, new GameObject("VoteCounter"+player.id));
 		}
 
+		// Placement des compteurs de vote dans l'écran de jeu
 		voteCounter ["Y"].transform.position = new Vector3 (-59, 69, 0);
 		voteCounter ["B"].transform.position = new Vector3 (59, 69, 0);
 		voteCounter ["A"].transform.position = new Vector3 (59, -69, 0);
@@ -62,30 +74,36 @@ public class VoteState : MonoBehaviour {
 
 	}
 
+	// Fonction d'affichage des votes dans les compteurs
 	void AddVote(Player _player){
-			foreach(var vote in voteCounter){
-				if(_player.hasVotedFor == vote.Key && _player.hasAlreadyVoted == false){
-				voteObject.Add (_player.id,Instantiate(Resources.Load ("skullVote")) as GameObject);
-				voteObject[_player.id].transform.SetParent (vote.Value.transform);
-				voteObject[_player.id].transform.position = vote.Value.transform.position + new Vector3((_player.id+1)*30,0,0);
+		// Pour chaque compteur dans voteCounter
+			foreach(var counter in voteCounter){
+			// Si un joueur a voté pour un joueur et qu'il n'avait pas voté avant...
+				if(_player.hasVotedFor == counter.Key && _player.hasAlreadyVoted == false){
+				// Création d'un crane et placement dans le compteur
+				voteSkull.Add (_player.id,Instantiate(Resources.Load ("skullVote")) as GameObject);
+				voteSkull[_player.id].transform.SetParent (counter.Value.transform);
+				voteSkull[_player.id].transform.position = counter.Value.transform.position + new Vector3((_player.id+1)*30,0,0);
+				// Le joueur a donc voté
 				_player.hasAlreadyVoted = true;
 				}
-				else if(_player.hasVotedFor == vote.Key && _player.hasAlreadyVoted == true){
-				voteObject[_player.id].transform.SetParent (vote.Value.transform);
-				voteObject[_player.id].transform.position = vote.Value.transform.position + new Vector3((_player.id+1)*30,0,0);
+			// Si le joueur change son vote (s'il a déjà voté mais qu'il vote encore une fois)
+				else if(_player.hasVotedFor == counter.Key && _player.hasAlreadyVoted == true){
+				// Changement de la place du crane vers le joueur pour lequel il change son vote
+				voteSkull[_player.id].transform.SetParent (counter.Value.transform);
+				voteSkull[_player.id].transform.position = counter.Value.transform.position + new Vector3((_player.id+1)*30,0,0);
 				}
 		} 
 		
 
 	}
 
-	// Comptage des votes à la fin de la manche
+	// Comptage des votes à la fin du vote
 	void countVotes(){
 		votesCounted = false;
 
 		// Comptage des votes pour chaque joueur
 		foreach(Player player in game.players){
-			Debug.Log (player.hasVotedFor);
 			if(player.hasVotedFor == "A" && votes.ContainsKey("A")){
 				votes["A"]+=1;
 			}else if(player.hasVotedFor == "B" && votes.ContainsKey("B")){
@@ -109,7 +127,8 @@ public class VoteState : MonoBehaviour {
 					Debug.Log (" -Joueur # "+player.id+" a reçu "+player.hasVotes+" votes contre lui.");
 
 
-					// Comptage des scores
+					// Comptage des scores, différentes conditions de victoire et défaite
+
 					// Si le joueur a >3 votes et n'était pas résistant, score /2
 					if(player.hasVotes > 3 && !player.isResistant){
 						player.score = player.score /2;
@@ -150,13 +169,15 @@ public class VoteState : MonoBehaviour {
 
 					// Si le bouton != lui-meme & que ce n'est pas les touches fléchées
 					if(buttonName != player.voteID && new List<string>(){"A","B","X","Y"}.Contains(buttonName)){
-						// Attribution du vote TODO : FEEDBACK
 					player.hasVotedFor = buttonName;
-						AddVote (player);
+						// Création / déplacement des skulls dans les compteurs
+					AddVote (player);
 					Debug.Log ("Joueur "+player.id+" a voté pour "+player.hasVotedFor);
+
 						// Si le joueur vote pour lui-meme, NOPE TODO : FEEDBACK
 					}else if(buttonName == player.voteID){
 						Debug.Log("Vous ne pouvez pas voter contre vous-meme !");
+
 						// Si le joueur appuie sur une touche fléchée, NOPE TODO : FEEDBACK
 					}else if(new List<string>(){"Up", "Down", "Left", "Right"}.Contains(buttonName)){
 						Debug.Log ("Vote invalide !");
@@ -167,7 +188,7 @@ public class VoteState : MonoBehaviour {
 		}
 					
 		// A la fin du vote...
-		if (votesCounted == false && currentTime > voteDuration || hasVoted == 4 ) {
+		if (votesCounted == false && currentTime > voteDuration || hasVoted == 4) {
 			countVotes();
 		}
 
