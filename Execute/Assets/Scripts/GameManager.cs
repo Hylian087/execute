986e55c0dfd0748dc8ff5cb75c0477e9c79dea61
11501513 <etienne@gauvin.info>
2016-03-21 17:11:12 +0100

    Défilement des nuages

--- Execute/Assets/Scripts/GameManager.cs
+++ Execute/Assets/Scripts/GameManager.cs
@@ -16,6 +16,13 @@ public class GameManager : MonoBehaviour {
 	// Prefabs
 	public Dictionary<string, GameObject> prefabs;
 	
+	// Nuages
+	private GameObject clouds;
+	private int lastCloudId;
+	private float lastCloudTime = 0.0f;
+	private int lastCloudLayerOrder = 20;
+	private float cloudsTime = 0.0f; 
+	
 	/**
 	 * Retourne l'unique instance de GameManager
 	 * @return <GameManager>
@@ -48,6 +55,13 @@ public class GameManager : MonoBehaviour {
 		
 		// Prefabs des feedbacks
 		prefabs.Add("ButtonFeedbackOK", Resources.Load("ButtonFeedbackOK", typeof(GameObject)) as GameObject);
+		
+		// Chargement des prefabs des nuages
+		clouds = GameObject.Find("Clouds");
+		for (int i = 0; i < 3; i++) {
+			prefabs.Add("Cloud" + i, Resources.Load("Clouds/Cloud" + i, typeof(GameObject)) as GameObject);
+		}
+		
 	}
 	
 	/**
@@ -59,6 +73,7 @@ public class GameManager : MonoBehaviour {
 			joypads[i] = new Joypad(i);
 		}
 		
+		
 		// Lancement d'une partie
 		game = ExecGame.MakeExecGame();
 		
@@ -68,6 +83,58 @@ public class GameManager : MonoBehaviour {
 
 		//Update nécessaire pour le fonctionnement des Joypad
 		Joypad.UpdateAll();
+		
+		// Mise à jour de la position des nuages
+		UpdateClouds(Time.deltaTime);
+	}
+	
+	// Mise à jour des nuages
+	public void UpdateClouds(float deltaTime) {
+		int count = 0;
+		float lastCloudPosition = 0.0f;
+		
+		const float spawnY = 250f;
+		const float destroyY = -100f;
+		
+		foreach (Transform cloudTransform in clouds.transform) {
+			Vector3 pos = cloudTransform.position;
+			
+			pos.y -= 5 * deltaTime * (((pos.y + -destroyY) / (spawnY + -destroyY)) + 0.01f);
+			
+			cloudTransform.position = pos;
+			
+			if (pos.y < destroyY) {
+				Destroy(cloudTransform.gameObject);
+			}
+			
+			lastCloudPosition = Mathf.Max(pos.y, lastCloudPosition);
+			
+			count++;
+		}
+		
+		if (lastCloudPosition < spawnY - 100) {
+			int randId;
+			
+			do {
+				randId = Random.Range(0, 3);
+			} while (randId == lastCloudId);
+			lastCloudId = randId;
+			
+			GameObject cloud = Instantiate(prefabs["Cloud" + randId]);
+			Vector3 pos = cloud.transform.position;
+			
+			pos.x = Random.Range(-320f, 320f);
+			pos.y = spawnY;
+			
+			cloud.transform.position = pos;
+			cloud.transform.SetParent(clouds.transform);
+			lastCloudTime = cloudsTime;
+			
+			Debug.Log("Nuage " + lastCloudTime);
+			cloud.GetComponent<Renderer>().sortingOrder = ++lastCloudLayerOrder;
+		}
+		
+		cloudsTime += deltaTime;
 	}
 
 }
