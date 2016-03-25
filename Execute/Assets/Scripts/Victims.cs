58e84b07babb69ba02a24f629221a0f3f79e067f
Hylian087 <atlaswasright087@gmail.com>
2016-03-25 01:57:27 +0100

    Début d'animation des personnages entrant dans la machine

---
+++ Execute/Assets/Scripts/Victims.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Victims : MonoBehaviour {
+
+	public Dictionary<int, GameObject> characters = new Dictionary<int, GameObject>();
+	public int characterAmount = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+		for (int i = 0; i<characterAmount; i++) {
+			characters.Add (i,Instantiate(Resources.Load("Char0"+Random.Range(1,8)))as GameObject);
+			characters[i].transform.SetParent(gameObject.transform);
+			characters[i].transform.position = new Vector3(-120-(7*i),0,0);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
