45313a2e50c3b0aad22a7dfdf997b7cdcf792396
Étienne Gauvin <etienne@gauvin.info>
2016-03-29 07:02:41 +0200

    Légère correction de la synchronisation de l'intro avec la musique

--- Execute/Assets/Scripts/StartScreen/menuScript.cs
+++ Execute/Assets/Scripts/StartScreen/menuScript.cs
@@ -77,7 +77,9 @@ public class menuScript : MonoBehaviour {
 	}
 
 	IEnumerator introCine(){
-		yield return new WaitForSeconds (1.0f);
+		yield return new WaitForSeconds (2.0f);
+		GameObject.Find("MenuManager").GetComponent<AudioSource>().Play();
+		yield return new WaitForSeconds (0.5f);
 		introText1.SetActive (true);
 		yield return new WaitForSeconds (3.0f);
 		introText1.SetActive (false);
